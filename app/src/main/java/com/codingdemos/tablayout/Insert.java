d060947b93949f76cfcfd30184f903ff9a30d4dc
macbook <5810110140@psu.ac.th>
2019-04-17 21:37:29 +0700

    17 april 62

--- app/src/main/java/com/codingdemos/tablayout/Insert.java
+++ app/src/main/java/com/codingdemos/tablayout/Insert.java
@@ -3,51 +3,77 @@ package com.codingdemos.tablayout;
 import android.app.DatePickerDialog;
 
 import android.app.TimePickerDialog;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.Calendar;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class Insert extends AppCompatActivity {
         TextView mTdate, mTtime;
-        Button mBtdate, mBttime;
-
+        Button mBtdate, mBttime, mBtsave;
+        DatabaseReference reff;
+        Member member;
+        Spinner Spinnertype;
+        int date;
+        int month;
+        int year;
+        int mHour;
+        int mMinute;
         Calendar c;
         DatePickerDialog dpd;
         TimePickerDialog tpd;
+
 
         @Override
         protected void  onCreate(Bundle saveInstanceState) {
 
             super.onCreate(saveInstanceState);
+            FirebaseApp.initializeApp(this);
             setContentView(R.layout.activity_insert);
 
             mTdate = (TextView)findViewById(R.id.textdate);
             mTtime = (TextView)findViewById(R.id.texttime);
+            Spinnertype = (Spinner)findViewById(R.id.spinner2);
             mBtdate = (Button)findViewById(R.id.btndate);
             mBttime = (Button)findViewById(R.id.btntime);
+            mBtsave = (Button)findViewById(R.id.btsave);
+            member = new Member();
+            reff = FirebaseDatabase.getInstance().getReference().child("Member");
+
+
+            final String[] typemushroom = getResources().getStringArray(R.array.mushroom);
+            final ArrayAdapter<String> adaptermushroom = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_dropdown_item_1line, typemushroom);
+            Spinnertype.setAdapter(adaptermushroom);
 
             mBtdate.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View view) {
                     c = Calendar.getInstance();
-                    int day = c.get(Calendar.DAY_OF_MONTH);
-                    int month = c.get(Calendar.MONTH);
-                    int year = c.get(Calendar.YEAR);
+                     date = c.get(Calendar.DATE);
+                     month = c.get(Calendar.MONTH);
+                     year = c.get(Calendar.YEAR);
 
                     dpd = new DatePickerDialog(Insert.this, new DatePickerDialog.OnDateSetListener() {
                         @Override
                         public void onDateSet(DatePicker view, int mYear, int mMonth, int mDay) {
-                            mTdate.setText(mDay + "/" + (mMonth+1)+ "/" + mYear);
+                            mTdate.setText(mDay + "/" + (mMonth)+ "/" + mYear);
                         }
-                    },day, month, year);
+                    }, date, month, year);
                     dpd.show();
                 }
             });
@@ -55,8 +81,8 @@ public class Insert extends AppCompatActivity {
                 @Override
                 public void onClick(View view){
                     final Calendar c = Calendar.getInstance();
-                    int mHour = c.get(Calendar.HOUR_OF_DAY);
-                    int mMinute = c.get(Calendar.MINUTE);
+                     mHour = c.get(Calendar.HOUR);
+                     mMinute = c.get(Calendar.MINUTE);
 
                     tpd = new TimePickerDialog(Insert.this, new TimePickerDialog.OnTimeSetListener() {
                         @Override
@@ -67,6 +93,30 @@ public class Insert extends AppCompatActivity {
                     tpd.show();
                 }
             });
+           mBtsave.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    /*date = (int) Integer.parseInt(String.valueOf(mBtdate.getText()));
+                    month = (int) Integer.parseInt(String.valueOf(mBtdate.getText()));
+                    year = (int) Integer.parseInt(String.valueOf(mBtdate.getText()));*/
+
+                    /*mHour = (int) Integer.parseInt(String.valueOf(mBttime.getText().length()));
+                    mMinute = (int) Integer.parseInt(String.valueOf(mBttime.getText().length()));*/
+                    typemushroom = Integer.parseInt(Spinnertype.getTextAlignment())
+                    member.setDate(date);
+                    member.setMonth(month);
+                    member.setYear(year);
+
+                    member.setHour(mHour);
+                    member.setMinute(mMinute);
+                    member.setTypemushroom(typemushroom);
+                    reff.push().setValue(member);
+                    Toast.makeText(Insert.this," data inserted sucessfully" ,Toast.LENGTH_LONG);
+
+                }
+            });
+
+
 
         }
 }
---
+++ app/src/main/java/com/codingdemos/tablayout/Member.java
@@ -0,0 +1,63 @@
+ package com.codingdemos.tablayout;
+
+ public class Member {
+
+    private int date;
+    private  int month;
+    private  int year;
+    private int Hour;
+    private int minute;
+    private String typemushroom;
+
+    public Member() {
+
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getHour() {
+        return Hour;
+    }
+
+    public void setHour(int hour) {
+        this.Hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+     public String getTypemushroom() {
+         return typemushroom;
+     }
+
+     public void setTypemushroom(String typemushroom) {
+         this.typemushroom = typemushroom;
+     }
+ }
